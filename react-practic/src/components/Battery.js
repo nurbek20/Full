08c8da9093b1ed4a0f2053af14abef45a5c5a70e
nurbek20 <nurbek19957072@gmail.com>
2022-10-25 18:05:50 +0600

    comment

--- react-practic/src/components/Battery.js
+++ react-practic/src/components/Battery.js
@@ -44,3 +44,31 @@ class Battery extends Component {
 }
 
 export default Battery;
+// import React, { useEffect, useState } from 'react';
+// import "./Battery.css";
+
+// const Battery = () => {
+//     const [count, setCount] = useState(0)
+//     let color = 'red'
+//     count >= 2 ? (count === 3 ? color = 'green' : color = 'yellow') : color = 'red'
+//     useEffect(() => {
+//         let int = setInterval(() => {
+//             setCount(count => count === 3 ? setCount(0) : setCount(count + 1))
+//         }, 1000)
+//         return () => clearInterval(int)
+//     },[])
+//     return (
+//         <>
+//             <section className={"batery"}>
+//                 <div className={"batery__box"}>
+//                     <div style={{ backgroundColor: color }} className={count >= 1 ? 'box1' : ''}></div>
+//                     <div style={{ backgroundColor: color }} className={count >= 2 ? 'box2' : ''}></div>
+//                     <div style={{ backgroundColor: color }} className={count >= 3 ? 'box3' : ''}></div>
+//                 </div>
+//                 <div className={"box"}></div>
+//             </section>
+//         </>
+//     )
+// }
+
+// export default Battery;
